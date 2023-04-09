742a59721acd76475ec0dbdf71833aba3a0dc457
Jerry Yang <efjerryyang@outlook.com>
2023-04-09 22:52:54 +0800

    fix: client side `Cross-Origin Request Blocked` error

--- gomoku-online/src/main/java/me/efjerryyang/gomokuonline/CorsConfiguration.java
+++ gomoku-online/src/main/java/me/efjerryyang/gomokuonline/CorsConfiguration.java
@@ -1,2 +1,25 @@
-package me.efjerryyang.gomokuonline;public class CorsConfiguration {
+package me.efjerryyang.gomokuonline;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@CrossOrigin(origins = "http://localhost:5174", allowCredentials = "true")
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5174", "http://127.0.0.1:5174")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
--- gomoku-online/src/main/java/me/efjerryyang/gomokuonline/SecurityConfiguration.java
+++ gomoku-online/src/main/java/me/efjerryyang/gomokuonline/SecurityConfiguration.java
@@ -1,2 +1,18 @@
-package me.efjerryyang.gomokuonline;public class SecurityConfiguration {
+package me.efjerryyang.gomokuonline;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration  {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)  throws  Exception{
+        return http
+                .cors().and()
+                .csrf().disable()
+                .build();
+    }
 }
