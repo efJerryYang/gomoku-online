cdf451dad18d7d408c9adda6a9dbeb017a3db6b0
Jerry Yang <efjerryyang@outlook.com>
2023-04-09 20:50:52 +0800

    feat: initialize spring boot project

---
+++ gomoku-online/src/main/java/me/efjerryyang/gomokuonline/GomokuOnlineApplication.java
@@ -0,0 +1,13 @@
+package me.efjerryyang.gomokuonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GomokuOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GomokuOnlineApplication.class, args);
+	}
+
+}
---
+++ gomoku-online/src/test/java/me/efjerryyang/gomokuonline/GomokuOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package me.efjerryyang.gomokuonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GomokuOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
