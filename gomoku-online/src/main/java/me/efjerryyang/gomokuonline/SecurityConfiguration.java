a8f65e3b5a97bb782cc256967385421991e06ec7
Jerry Yang <efjerryyang@outlook.com>
2023-04-09 22:59:55 +0800

    style: reformat code using IDEA

--- gomoku-online/src/main/java/me/efjerryyang/gomokuonline/SecurityConfiguration.java
+++ gomoku-online/src/main/java/me/efjerryyang/gomokuonline/SecurityConfiguration.java
@@ -3,13 +3,12 @@ package me.efjerryyang.gomokuonline;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfiguration  {
+public class SecurityConfiguration {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)  throws  Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .cors().and()
                 .csrf().disable()
