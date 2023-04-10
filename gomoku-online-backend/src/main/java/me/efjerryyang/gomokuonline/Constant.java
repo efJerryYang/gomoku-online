42bbdaec2cdd3d7e9800fa0b57dfda953546dfca
Jerry Yang <efjerryyang@outlook.com>
2023-04-10 15:33:18 +0800

    feat(backend): add user status constants

---
+++ gomoku-online-backend/src/main/java/me/efjerryyang/gomokuonline/Constant.java
@@ -0,0 +1,10 @@
+package me.efjerryyang.gomokuonline;
+
+
+public class Constant {
+    // User status
+    public static final int USER_STATUS_WAITING = 0;
+    public static final int USER_STATUS_MATCHED = 1;
+    public static final int USER_STATUS_PLAYING = 2;
+    public static final int USER_STATUS_OFFLINE = 3;
+}
