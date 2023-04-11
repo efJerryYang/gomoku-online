210fcc2194c05c81099873a48b4e930e12af73d0
Jerry Yang <efjerryyang@outlook.com>
2023-04-11 10:47:24 +0800

    feat: allow direct confirmation of match

--- gomoku-online-backend/src/main/java/me/efjerryyang/gomokuonline/dto/GameDTO.java
+++ gomoku-online-backend/src/main/java/me/efjerryyang/gomokuonline/dto/GameDTO.java
@@ -4,22 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import me.efjerryyang.gomokuonline.entity.Game;
+import me.efjerryyang.gomokuonline.entity.Player;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class GameDTO {
     private Long id;
-    private String player1;
-    private String player2;
+    private Player player1;
+    private Player player2;
     private Integer turn;
     private Integer[][] board;
     private Integer result;
 
     public GameDTO(Game game) {
         this.id = game.getId();
-        this.player1 = game.getPlayer1().getUsername();
-        this.player2 = game.getPlayer2().getUsername();
+        this.player1 = game.getPlayer1();
+        this.player2 = game.getPlayer2();
         this.turn = game.getTurn();
         this.board = game.getBoard();
         this.result = game.getStatus();
