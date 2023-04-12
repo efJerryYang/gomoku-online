99aba1a12ffafd65987f5829c766c3e081581c67
Jerry Yang <efjerryyang@outlook.com>
2023-04-12 21:28:38 +0800

    feat: add dice to match a random user

---
+++ gomoku-online-backend/src/main/java/me/efjerryyang/gomokuonline/dto/DiceDTO.java
@@ -0,0 +1,13 @@
+package me.efjerryyang.gomokuonline.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DiceDTO {
+    private Long userId;
+
+}
