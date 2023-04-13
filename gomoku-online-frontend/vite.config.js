e7c55fbab5f957a8ea31244d76fb2dac9d62a135
Jerry Yang <efjerryyang@outlook.com>
2023-04-13 13:31:25 +0800

    chore: disable hard-coded IP addresses, use localhost

--- gomoku-online-frontend/vite.config.js
+++ gomoku-online-frontend/vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
     port: 8081,
     proxy: {
       '/api': {
-        target: 'http://10.250.123.222:8080/',
+        target: 'http://localhost:8080/',
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, '')
       }
